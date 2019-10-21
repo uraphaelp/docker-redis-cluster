c659fbaf6ee30257c35e1eb9c9222949e31e9ebe
zhenghaowei <zhenghaowei@corp.netease.com>
2019-10-21 11:51:45 +0800

    feat:解耦各种集群模式构建

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -14,23 +14,37 @@ if [ "$1" = 'redis-cluster' ]; then
     IP=${IP%% *} # use the first ip
     echo " -- IP After trim: '$IP'"
 
-    if [ -z "$INITIAL_PORT" ]; then # Default to port 7000
+    # Default to port 7000
+    if [ -z "$INITIAL_PORT" ]; then
       INITIAL_PORT=7000
+    else
+      INITIAL_PORT=$INITIAL_PORT
     fi
 
-    if [ -z "$MASTERS" ]; then # Default to 3 masters
+    # Default to 3 masters
+    if [  -z "$SLAVES_PER_MASTER" && "$SENTINEL"!="true" && "$STANDALONE"!="true" ]; then
       MASTERS=3
+    elif [ -z "$MASTERS" && "$SENTINEL"="true" || "$STANDALONE"="true" ]; then
+      MASTERS=1
+    else
+      MASTERS=$MASTERS
     fi
 
-    if [ -z "$SLAVES_PER_MASTER" ]; then # Default to 1 slave for each master
+    # Default to 1 slave for each master
+    if [ -z "$SLAVES_PER_MASTER" && "$SENTINEL"!="true" && "$STANDALONE"!="true" ]; then
       SLAVES_PER_MASTER=1
+    elif [ -z "$SLAVES_PER_MASTER" && "$SENTINEL"="true" ]; then
+      SLAVES_PER_MASTER=2
+    elif [ -z "$SLAVES_PER_MASTER" && "$STANDALONE"="true" ]; then
+      SLAVES_PER_MASTER=0
+    else
+      SLAVES_PER_MASTER=$SLAVES_PER_MASTER
     fi
+
+    # TODO:当至少出现两种集群模式为 true 时，抛错
+
 
     max_port=$(($INITIAL_PORT + $MASTERS * ( $SLAVES_PER_MASTER  + 1 ) - 1))
-    first_standalone=$(($max_port + 1))
-    if [ "$STANDALONE" = "true" ]; then
-      max_port=$(($max_port + 2))
-    fi
 
     for port in $(seq $INITIAL_PORT $max_port); do
       mkdir -p /redis-conf/${port}
@@ -48,16 +62,16 @@ if [ "$1" = 'redis-cluster' ]; then
         rm /redis-data/${port}/appendonly.aof
       fi
 
-      if [ "$port" -lt "$first_standalone" ]; then
+      if [ "$STANDALONE"="true" || "$SENTINEL"="true" ]; then
+        PORT=${port} envsubst < /redis-conf/redis.tmpl > /redis-conf/${port}/redis.conf
+      else
         PORT=${port} envsubst < /redis-conf/redis-cluster.tmpl > /redis-conf/${port}/redis.conf
         nodes="$nodes $IP:$port"
-      else
-        PORT=${port} envsubst < /redis-conf/redis.tmpl > /redis-conf/${port}/redis.conf
       fi
 
       if [ "$port" -lt $(($INITIAL_PORT + $MASTERS)) ]; then
         if [ "$SENTINEL" = "true" ]; then
-          PORT=${port} SENTINEL_PORT=$((port - 2000)) envsubst < /redis-conf/sentinel.tmpl > /redis-conf/sentinel-${port}.conf
+          PORT=${port} SENTINEL_PORT=$((port + 2000)) envsubst < /redis-conf/sentinel.tmpl > /redis-conf/sentinel-${port}.conf
           cat /redis-conf/sentinel-${port}.conf
         fi
       fi
@@ -71,13 +85,20 @@ if [ "$1" = 'redis-cluster' ]; then
 
     /redis/src/redis-cli --version | grep -E "redis-cli 3.0|redis-cli 3.2|redis-cli 4.0"
 
-    if [ $? -eq 0 ]
-    then
+    #TODO: 针对 standalone 和 sentinel 避免创建 cluster
+    # 针对不同版本创建，使用不同的创建工具
+    if [ $? -eq 0 && "$SENTINEL"!="true" && "$STANDALONE"!="true" ]; then
       echo "Using old redis-trib.rb to create the cluster"
       echo "yes" | eval ruby /redis/src/redis-trib.rb create --replicas "$SLAVES_PER_MASTER" "$nodes"
-    else
+    elif [ $? -ne 0 && "$SENTINEL"!="true" && "$STANDALONE"!="true" ]; then
       echo "Using redis-cli to create the cluster"
       echo "yes" | eval /redis/src/redis-cli --cluster create --cluster-replicas "$SLAVES_PER_MASTER" "$nodes"
+    fi
+
+    if [ "$STANDALONE"="true" ]; then
+      for port in $(seq $INITIAL_PORT $(($INITIAL_PORT + $MASTERS))); do
+        redis-server /redis-conf/${port}/redis.conf &
+      done
     fi
 
     if [ "$SENTINEL" = "true" ]; then
